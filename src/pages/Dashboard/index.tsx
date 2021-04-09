9ecb618fb937feaa0bcb554b3ac44299d9bd46a3
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-09 17:03:43 -0300

    dashboard: list providers and buttons working

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,22 +1,109 @@
-import React from 'react';
-import { View, Button } from 'react-native';
-import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, Image } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/Auth';
+import api from '../../services/api';
+
+import {
+    Container,
+    Header,
+    HeaderTitle,
+    UserName,
+    ProfileButton,
+    UserAvatar,
+    ProvidersList,
+    ProvidersListTitle,
+    ProviderContainer,
+    ProviderAvatar,
+    ProviderInfo,
+    ProviderName,
+    ProviderMeta,
+    ProviderMetaText,
+} from './styles';
+
+export interface Provider {
+    id: string;
+    name: string;
+    avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-    const { signOut } = useAuth();
+    const [providers, setProvider] = useState<Provider[]>([]);
+    const { user, signOut } = useAuth();
+    const { navigate } = useNavigation();
+
+    useEffect(() => {
+        api.get<Provider[]>('providers').then(response => {
+            setProvider(response.data);
+        });
+    }, []);
+
+    const navigateToProfile = useCallback(() => {
+        // navigate('Profile');
+        signOut();
+    }, [signOut]);
+
+    const navigateToCreateAppointment = useCallback(
+        (providerId: string) => {
+            navigate('CreateAppointment', { providerId });
+        },
+        [navigate],
+    );
 
     return (
-        <View
-            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        >
-            <Container>
-                <Button title="Sair" onPress={signOut}>
-                    Sign Out
-                </Button>
-            </Container>
-        </View>
+        <Container>
+            <Header>
+                <HeaderTitle>
+                    Bem vindo, {'\n'}
+                    <UserName>{user.name}</UserName>
+                </HeaderTitle>
+                <ProfileButton onPress={navigateToProfile}>
+                    <UserAvatar source={{ uri: user.avatar_url }} />
+                </ProfileButton>
+            </Header>
+            <ProvidersList
+                data={providers}
+                keyExtractor={provider => provider.id}
+                ListHeaderComponent={
+                    <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+                }
+                renderItem={({ item: provider }) => (
+                    <ProviderContainer
+                        onPress={() => {
+                            navigateToCreateAppointment(provider.id);
+                        }}
+                    >
+                        <ProviderAvatar
+                            source={{
+                                uri: provider.avatar_url
+                                    ? provider.avatar_url
+                                    : 'https://source.unsplash.com/random',
+                            }}
+                        />
+                        <ProviderInfo>
+                            <ProviderName>{provider.name}</ProviderName>
+                            <ProviderMeta>
+                                <Icon
+                                    name="calendar"
+                                    size={14}
+                                    color="#ff9000"
+                                />
+                                <ProviderMetaText>
+                                    Segunda à Sexta
+                                </ProviderMetaText>
+                            </ProviderMeta>
+                            <ProviderMeta>
+                                <Icon name="clock" size={14} color="#ff9000" />
+                                <ProviderMetaText>8h às 18h</ProviderMetaText>
+                            </ProviderMeta>
+                        </ProviderInfo>
+                    </ProviderContainer>
+                )}
+            />
+        </Container>
     );
 };
 
