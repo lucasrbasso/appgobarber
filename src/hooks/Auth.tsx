2c0396b82552024e8567af15e68507e419f0660f
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-12 20:03:30 -0300

    hooks:  user is now logged out after a day

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -6,6 +6,7 @@ import React, {
     useEffect,
 } from 'react';
 
+import { differenceInDays, parseISO } from 'date-fns';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
@@ -32,6 +33,7 @@ interface AuthContextData {
     loading: boolean;
     signIn: (credentials: SignInCredentials) => Promise<void>;
     signOut: () => void;
+    updateUser: (user: User) => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -40,23 +42,18 @@ const AuthProvider: React.FC = ({ children }) => {
     const [data, setData] = useState<AuthState>({} as AuthState);
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function loadStoragedData(): Promise<void> {
-            const [token, user] = await AsyncStorage.multiGet([
-                '@GoBarber:token',
-                '@GoBarber:user',
-            ]);
+    const updateUser = useCallback(
+        async (user: User) => {
+            await AsyncStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
-            if (token[1] && user[1]) {
-                api.defaults.headers.authorization = `Bearer ${token[1]}`;
+            setData({
+                token: data.token,
+                user,
+            });
+        },
 
-                setData({ token: token[1], user: JSON.parse(user[1]) });
-            }
-            setLoading(false);
-        }
-
-        loadStoragedData();
-    }, []);
+        [setData, data.token],
+    );
 
     const signIn = useCallback(
         async ({ email, password }: SignInCredentials) => {
@@ -67,6 +64,7 @@ const AuthProvider: React.FC = ({ children }) => {
             await AsyncStorage.multiSet([
                 ['@GoBarber:token', token],
                 ['@GoBarber:user', JSON.stringify(user)],
+                ['@GoBarber:date', JSON.stringify(new Date())],
             ]);
 
             api.defaults.headers.authorization = `Bearer ${token}`;
@@ -77,14 +75,46 @@ const AuthProvider: React.FC = ({ children }) => {
     );
 
     const signOut = useCallback(async () => {
-        await AsyncStorage.multiRemove(['@GoBarber:token', '@GoBarber:user']);
+        await AsyncStorage.multiRemove([
+            '@GoBarber:token',
+            '@GoBarber:user',
+            '@GoBarber:date',
+        ]);
 
         setData({} as AuthState);
     }, []);
 
+    useEffect(() => {
+        async function loadStoragedData(): Promise<void> {
+            const [token, user, dateLogged] = await AsyncStorage.multiGet([
+                '@GoBarber:token',
+                '@GoBarber:user',
+                '@GoBarber:date',
+            ]);
+
+            if (token[1] && user[1]) {
+                api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
+                setData({ token: token[1], user: JSON.parse(user[1]) });
+            }
+
+            if (dateLogged[1]) {
+                const date = parseISO(JSON.parse(dateLogged[1]));
+
+                if (differenceInDays(new Date(Date.now()), date) >= 1) {
+                    signOut();
+                }
+            }
+
+            setLoading(false);
+        }
+
+        loadStoragedData();
+    }, [signOut]);
+
     return (
         <AuthContext.Provider
-            value={{ user: data.user, loading, signIn, signOut }}
+            value={{ user: data.user, loading, signIn, signOut, updateUser }}
         >
             {children}
         </AuthContext.Provider>
